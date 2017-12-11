284ff5c5fad18246652b8c67bd4a4d827d7cb700
aleks <amd.lan>
2017-12-11 16:34:09 +0300

    add footer.css

--- api/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from rest_framework.authtoken.admin import TokenAdmin
-
-TokenAdmin.raw_id_fields = ('user',)
