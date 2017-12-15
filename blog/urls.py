5bcde9e869a81df32236091e132bb664923b53a2
kylikov aleks <aleks@samsung.lan>
2017-12-15 21:43:54 +0300

    add cashe url blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,10 @@
 
 from django.conf.urls import url
 from blog.views import PostsListView, PostDetailView
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    url(r'^$', PostsListView.as_view(), name='Postlist'),
-    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='Detallist'),
-    
-    
+    url(r'^$', cache_page(60 * 15)(PostsListView.as_view()), name='Postlist'),
+    url(r'^(?P<pk>\d+)/$', cache_page(60 * 15)(PostDetailView.as_view()), name='Detallist'),
 ]
