cbb0d59cadcdc943e30c8eb87812aefed790417e
kylikov aleks <aleks@samsung.lan>
2017-12-18 22:51:06 +0300

    исправлено api

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class SerPost(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('url', 'autor', 'title', 'content', 'datetime')
+        fields = ('url', 'title', 'content', 'date_create')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
