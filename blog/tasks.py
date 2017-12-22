b275caef4a0dccd3ce929a6f1bd9f4656c01bc2d
aleks <amd.lan>
2017-12-22 09:45:45 +0300

    убран celery task из blog app

--- blog/tasks.py
+++ blog/tasks.py
@@ -4,6 +4,3 @@ from celery.task import periodic_task
 from django.contrib.sessions.models import Session
 from datetime import datetime
 
-@periodic_task(ignore_result=True, run_every=crontab(hour=16, minute=33))
-def clean_sessions():
-    Session.objects.filter(expire_date__lt=datetime.now()).delete()
