4589843bb3275e1d5164303e028de270fcd02d44
aleks <amd.lan>
2017-12-22 09:33:44 +0300

    очистка кеша

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
+from celery.task import periodic_task
+from django.core.cache import cache
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -15,4 +19,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+@periodic_task(ignore_result=True, run_every=crontab(hour=0, minute=0))
+def clean_cache():
+    cache.clear()
+
 
