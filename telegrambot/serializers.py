7ebc801ff587e2c8a3893192d60ce8625e7fa2e8
aleks <amd.lan>
2017-12-23 09:18:40 +0300

    rttrx

--- telegrambot/serializers.py
+++ telegrambot/serializers.py
@@ -39,7 +39,7 @@ class MessageSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Message
-        field = __all__
+        field = ( 'from_user' ,'date' , 'chat' , 'forward_from' ,'text')
 
 
 class UpdateSerializer(serializers.HyperlinkedModelSerializer):
