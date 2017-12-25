a0bc327364a5542ab6df92da39304230a5d95ba9
kylikov aleks <aleks@samsung.lan>
2017-12-25 23:07:40 +0300

    add loging

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -4,14 +4,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from telegrambot.serializers import UpdateSerializer
 from rest_framework import status
+import logging
 
+logger = logging.getLogger(__name__)
 
 class WebhookView(APIView):
     def post(self, request, token):
+        logger.info(request.data)
         serializer = UpdateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-            
